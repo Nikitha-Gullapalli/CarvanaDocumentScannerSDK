a94603e01a36b9791ff90648090740528bbd33e2
NGullapa <nikitha.gullapalli@carvana.com>
2025-07-17 13:35:11 -0700

    Clean Package.swift - remove phantom dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-1// swift-tools-version: 5.9
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -15,9 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ComposeApp",
-            // Update this URL to your actual GitHub repository or CDN
             url: "https://github.com/Nikitha-Gullapalli/CarvanaDocumentScannerSDK-/releases/download/v1.0.7/ComposeApp.xcframework.zip",
-            // This checksum will be generated when building the XCFramework
             checksum: "9e280c8f68981c9e9876eb174a5077cef80847c3a57256d2deab9f6ce956380e"
         )
     ]
