c1929e7819ee62af78941eba97fa9de87044963e
NGullapa <nikitha.gullapalli@carvana.com>
2025-07-17 11:22:19 -0700

    Update Package.swift with correct GitHub repository URL

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "ComposeApp",
             // Update this URL to your actual GitHub repository or CDN
-            url: "https://github.com/carvana/CarvanaDocumentScannerSDK/releases/download/1.0.7/ComposeApp.xcframework.zip",
+            url: "https://github.com/Nikitha-Gullapalli/CarvanaDocumentScannerSDK-/releases/download/v1.0.7/ComposeApp.xcframework.zip",
             // This checksum will be generated when building the XCFramework
             checksum: "9e280c8f68981c9e9876eb174a5077cef80847c3a57256d2deab9f6ce956380e"
         )
